632e151ec0664cf60797004f9bb2e186aee80397
Martin Bech <MartinJBech@live.com>
2026-09-04 16:23:42 +0200

    Remove unused core tree operations

--- src/DailyNagger.Mobile/src/services/core-tree-operations/contracts.ts
+++
@@ -1,7 +0,0 @@
-import type { Tree } from "@/models";
-
-export type TreeReader = {
-  readonly read: {
-    readonly getTree: () => Tree;
-  };
-};
--- src/DailyNagger.Mobile/src/services/core-tree-operations/index.ts
+++
@@ -1,5 +0,0 @@
-export { treeMutationOperations } from "./treeMutationOperations";
-export { selectedPathOperations } from "./selectedPathOperations";
-export { treeReadOperations } from "./treeReadOperations";
-export { branchOperations } from "./branchOperations";
-export type { TreeReader } from "./contracts";
