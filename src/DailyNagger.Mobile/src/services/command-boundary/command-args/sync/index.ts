e5a86a9ebf2ae2c012404220582a14bf64740db1
Martin Bech <MartinJBech@live.com>
2026-09-08 15:29:56 +0200

    Remove obsolete command boundary handlers

--- src/DailyNagger.Mobile/src/services/command-boundary/command-args/sync/index.ts
+++
@@ -1,5 +0,0 @@
-import type { Nagger } from "@/models";
-
-export type NaggerPinningArgs = {
-  readonly nagger: Nagger;
-};
--- src/DailyNagger.Mobile/src/services/command-boundary/commandArgs.ts
+++
@@ -1,5 +0,0 @@
-export * from "./command-args/editor";
-export * from "./command-args/editor-session";
-export * from "./command-args/navigation";
-export * from "./command-args/sync";
-export * from "./command-args/task-input";
